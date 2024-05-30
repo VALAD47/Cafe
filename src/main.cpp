494fdf0a6a087abb81681cd85b468ac16c97dac7
VALAD47 <valad.racz@gmail.com>
2024-05-30 18:10:55 +0300

    Added options switcher

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,14 @@ class Window
 private:
     WINDOW* win;
     Text selectables[32];
-    int opt = 0;
+    int opt = 0, selected = 0;
+
+    void highlight(short color_pair, Text text){
+        wattron(win, COLOR_PAIR(color_pair));
+        wprint(win, text);
+        wattroff(win, COLOR_PAIR(color_pair));
+    }
+
 public:
     Window(int heigh, int width){
         int x, y;
@@ -33,16 +40,34 @@ public:
         selectables[opt] = text;
         opt++;
         print(text);
+        if(opt-1==0) highlight(1, text);
     }
 
     void print(Text text){
         wprint(win, text);
     }
 
-    void select(Text text){
-        wattron(win, COLOR_PAIR(1));
-        print(text);
-        wattroff(win, COLOR_PAIR(1));
+    void select(int option){
+        switch (option){
+        case 1:
+            if(selected==0) return;
+            selected--;
+            highlight(1, selectables[selected]);
+            print(selectables[selected+1]);
+
+            break;
+        case 0:
+            if(opt-1==selected) return;
+            selected++;
+            highlight(1, selectables[selected]);
+            print(selectables[selected-1]);
+
+            break;
+        default:
+            break;
+        }
+        mvprintw(0,0,"Options: %d\tSelected: %d", opt, selected);
+        refresh();
     }
 };
 
@@ -52,15 +77,27 @@ int main(int argc,char** argv){
 
     init_pair(1, COLOR_WHITE, COLOR_CYAN);
     Window main(26, 36);
-    Text text1 = {"My first text!", 2, 3};
     refresh();
-    main.add_opt(text1);
+    main.add_opt({"My first text!", 2, 3});
+    main.add_opt({"My second text!", 3, 3});
+    main.add_opt({"My second text!", 4, 3});
     main.refresh();
-    getch();
-
-    main.select(text1);
-    main.refresh();
-    getch();
-
-    endwin();
+    keypad(stdscr, true);
+    while (true)
+    {
+        int i = getch();
+        switch (i)
+        {
+        case KEY_DOWN:
+            main.select(0);
+            break;
+        case KEY_UP:
+            main.select(1);
+            break;
+        default:
+            endwin();
+            return 0;
+            break;
+        }
+    }
 }
